6326f53ff53b04931236d3511bc81f792ef65b2d
Srivalli Korada <128020662+sriKorada@users.noreply.github.com>
2024-09-25 19:29:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+  const navbar = document.getElementById('navbar');
+  if (window.pageYOffset > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+};
+
